657d0c1065084d7152aee8185527ccee170873e6
Emiliano Volpino <volpinoemiliano@gmail.com>
2023-09-02 19:03:43 -0300

    3er Pre-Entrega

--- js/carrito.js
+++ js/carrito.js
@@ -1,111 +1,183 @@
 import { generarProductos, productosDisponibles } from "./inicio.js";
 
-JSON.parse(sessionStorage.getItem("carrito")) === null && sessionStorage.setItem("carrito", JSON.stringify([]))
+JSON.parse(sessionStorage.getItem("carrito")) === null &&
+    sessionStorage.setItem("carrito", JSON.stringify([]));
 
 document.addEventListener("DOMContentLoaded", () => {
-    generarCarrito()
-})
+    generarCarrito();
+});
 
-let carrito = JSON.parse(sessionStorage.getItem("carrito"))
-let listaCarrito = document.getElementById("items")
-let totalesCarrito = document.getElementById("totales")
+let carrito = JSON.parse(sessionStorage.getItem("carrito"));
+let listaCarrito = document.getElementById("items");
+let totalesCarrito = document.getElementById("totales");
+const btnCarrito = document.getElementById("botonCarrito");
+const carritoTabla = document.getElementById("carrito");
+
+btnCarrito.addEventListener("click", () => {
+    generarCarrito();
+    if (carritoTabla.style.display === "block") {
+        carritoTabla.style.display = "none";
+    } else {
+        carritoTabla.style.display = "block";
+    }
+});
 
 export const comprarProducto = (idProducto) => {
-    
-    const producto = productosDisponibles.find((producto) => producto.id === idProducto)
+    const producto = productosDisponibles.find(
+    (producto) => producto.id === idProducto
+    );
 
-    const { id, nombre, precio, imagen } = producto
+    const { id, nombre, precio, imagen } = producto;
 
-    const productoCarrito = carrito.find((producto) => producto.id === idProducto)
+    const productoCarrito = carrito.find(
+    (producto) => producto.id === idProducto
+    );
 
-    if(productoCarrito === undefined){
+    if (productoCarrito === undefined) {
         const nuevoProductoEnCarrito = {
             id: id,
             nombre: nombre,
             precio: precio,
             imagen: imagen,
-            cantidad: 1
-        }
+            cantidad: 1,
+        };
 
-        carrito.push(nuevoProductoEnCarrito)
-        sessionStorage.setItem("carrito", JSON.stringify(carrito))
+    carrito.push(nuevoProductoEnCarrito);
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    } else {
+        const indexProducto = carrito.findIndex(
+        (producto) => producto.id === idProducto
+        );
 
-    }else{
-        const indexProducto = carrito.findIndex((producto) => producto.id === idProducto)
+    carrito[indexProducto].cantidad++;
+    carrito[indexProducto].precio = precio * carrito[indexProducto].cantidad;
 
-        carrito[indexProducto].cantidad++
-        carrito[indexProducto].precio = precio * carrito[indexProducto].cantidad
-
-        sessionStorage.setItem("carrito", JSON.stringify(carrito))
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
     }
 
-    carrito = JSON.parse(sessionStorage.getItem("carrito"))
-
-}
+    carrito = JSON.parse(sessionStorage.getItem("carrito"));
+    alert('Producto agregado al carrito');
+    generarCarrito();
+};
 
 const generarCarrito = () => {
+    listaCarrito.innerHTML = '';
+    let cantidadTotal = 0;
+    let costoTotal = 0;
 
-    carrito.forEach(producto => {
-
-        const {id, imagen, nombre, cantidad, precio} = producto
-        
-        let body = document.createElement("tr")
-
-        body.className = "productoCarrito"
-
+    if (carrito.length === 0) {
+        const mensaje = document.createElement('tr');
+        mensaje.innerHTML = `
+        <td colspan="6">No hay productos en el carrito</td>
+        `;
+        listaCarrito.append(mensaje);
+    } else {
+        carrito.forEach((producto) => {
+        const { id, imagen, nombre, cantidad, precio } = producto;
+        let body = document.createElement('tr');
+        body.className = 'productoCarrito';
         body.innerHTML = `
-        <th><img id=fotoProductoEnCarrito src="${imagen}" class="card-img-top"></th>
-        <td>${nombre}</td>
-        <td>${cantidad}</td>
-        <td>$${precio}</td>
-        <td>
-        <button id="+${id}" class="btn btn-succes">+</button>
-        <button id="-${id}" class="btn btn-danger">-</button>
-        </td>
-        `
+            <th><img id=fotoProductoEnCarrito src="${imagen}" class="card-img-top" style="width:40%; height:30%"></th>
+            <td>${nombre}</td>
+            <td>${cantidad}</td>
+            <td>$${precio}</td>
+            <td>
+            <button id="+${id}" class="btn btn-success">+</button>
+            <button id="-${id}" class="btn btn-danger">-</button>
+            </td>
+        `;
+        listaCarrito.append(body);
 
-        listaCarrito.append(body)
+        const btnAgregar = document.getElementById(`+${id}`);
+        const btnSacar = document.getElementById(`-${id}`);
+        btnAgregar.addEventListener('click', () => aumentarCantidad(id));
+        btnSacar.addEventListener('click', () => disminuirCantidad(id));
 
-        const btnAgregar = document.getElementById(`+${id}`)
-        const btnSacar = document.getElementById(`-${id}`)
+        cantidadTotal += cantidad;
+        costoTotal += precio;
+        });
 
-        btnAgregar.addEventListener("click", () => console.log())
-        btnSacar.addEventListener("click", () => console.log())
+        document.getElementById('cantidadTotal').textContent = cantidadTotal;
+        document.getElementById('costoTotal').textContent = `$${costoTotal}`;
 
-    });
-
-    dibujarFooter()
-
+        generarTotales();
 }
+};
 
 const dibujarFooter = () => {
-
-    if(carrito.lenght > 0){
-        totalesCarrito.innerHTML = ""
-
-        let footer  = document.createElement("tr")
-
+    if (carrito.length > 0) {
+        totalesCarrito.innerHTML = '';
+        let footer = document.createElement('tr');
         footer.innerHTML = `
-        <th><b>Totales:</b></th>
-        <td></td>
-        <td>Cantidad</td>
-        <td></td>
-        <td>Total</td>
-        `
-
-        totalesCarrito.append(footer)
-    }else{
-        totalesCarrito.innerHTML = "<h3>No hay producto en carrito</h3>"
+            <th colspan="2"><b>Totales:</b></th>
+            <td id="cantidadTotal"></td>
+            <td></td>
+            <td id="costoTotal"></td>
+        `;
+        totalesCarrito.append(footer);
+    } else {
+        totalesCarrito.innerHTML = '<h3>No hay productos en el carrito</h3>';
     }
+    };
 
-}
+/*
+const generarTotales = () => {
+    const costoTotal = carrito.reduce((total, { precio }) => total + precio, 0);
+    const cantidadTotal = carrito.reduce(
+    (total, { cantidad }) => total + cantidad,0);
+
+    return {
+        costoTotal: costoTotal,
+        cantidadTotal: cantidadTotal,
+    };
+};
+*/
 
 const generarTotales = () => {
-    const costoTotal = carrito.reduce((total, {precio}) => total + precio, 0)
-    const cantidadTotal = carrito.reduce((total, {cantidad}) => total + cantidad, 0)
+    const { cantidadTotal, costoTotal } = calcularTotales();
+    document.getElementById('cantidadTotal').textContent = cantidadTotal;
+    document.getElementById('costoTotal').textContent = `$${costoTotal}`;
+};
 
-    return{
-        costoTotal: costoTotal,
-        cantidadTotal: cantidadTotal
+const calcularTotales = () => {
+    let cantidadTotal = 0;
+    let costoTotal = 0;
+    carrito.forEach((producto) => {
+    cantidadTotal += producto.cantidad;
+    costoTotal += producto.precio;
+    });
+    return {
+    cantidadTotal,
+    costoTotal,
+    };
+};
+
+const aumentarCantidad = (id) => {
+    const indexProducto = carrito.findIndex((producto) => producto.id === id);
+    const precio =
+    carrito[indexProducto].precio / carrito[indexProducto].cantidad;
+
+    carrito[indexProducto].cantidad++;
+    carrito[indexProducto].precio = precio * carrito[indexProducto].cantidad;
+
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+
+    generarCarrito();
+};
+
+const disminuirCantidad = (id) => {
+    const indexProducto = carrito.findIndex((producto) => producto.id === id);
+    const precio =
+    carrito[indexProducto].precio / carrito[indexProducto].cantidad;
+
+    carrito[indexProducto].cantidad--;
+    carrito[indexProducto].precio = precio * carrito[indexProducto].cantidad;
+
+    if (carrito[indexProducto].cantidad === 0) {
+    carrito.splice(indexProducto, 1);
     }
-}
+
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+
+    generarCarrito();
+};
